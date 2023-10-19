9d5337738b3e6cf4f33003c01b014c8a4a50253b
narobinson1 <nicolas.alexander.robinson.2@gmail.com>
2023-10-19 13:18:04 +0200

    update index url

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('utils/', include("utils.urls")),
+    path('', include("utils.urls")),
     # path('admin/', admin.site.urls),
 ]
